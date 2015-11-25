cc81b9fa1f84bf8eb37f2ba2361171e2fa7e395e
Jesse Huard <jhuard@ualberta.ca>
2015-11-24 22:02:42 -0700

    TEST: Add tests for computing indicators from TimeSeries.

--- test/test_timeseries.py
+++ test/test_timeseries.py
@@ -31,3 +31,29 @@ class TestTimeSeries(unittest.TestCase):
 
         self.assertItemsEqual(ts_file.times, ts_test.times)
         self.assertItemsEqual(ts_file.powers, ts_test.powers)
+
+    def test_indicators(self):
+        """Test getting the on-off indicators from a TimeSeries."""
+        ts = TimeSeries()
+        ts.array.resize(3)
+        ts.array[0] = (np.uint32(1), np.float32(0.0))
+        ts.array[1] = (np.uint32(2), np.float32(1.0))
+        ts.array[2] = (np.uint32(3), np.float32(0.0))
+
+        indicators_test = np.array([False, True, False])
+        indicators = ts.indicators()
+
+        self.assertItemsEqual(indicators, indicators_test)
+
+    def test_indicators_threshold(self):
+        """Test getting the on-off indicators with a threshold."""
+        ts = TimeSeries()
+        ts.array.resize(3)
+        ts.array[0] = (np.uint32(1), np.float32(0.0))
+        ts.array[1] = (np.uint32(2), np.float32(1.0))
+        ts.array[2] = (np.uint32(3), np.float32(2.0))
+
+        indicators_test = np.array([False, False, True])
+        indicators = ts.indicators(np.float32(1.1))
+
+        self.assertItemsEqual(indicators, indicators_test)
