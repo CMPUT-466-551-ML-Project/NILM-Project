f95d610457f60e3da53e82be91e5fe30404613cb
Jesse Huard <jhuard@ualberta.ca>
2015-11-26 12:51:43 -0700

    NILM: Fix formatting and pylint errors in network code.

--- nilm/network.py
+++ nilm/network.py
@@ -1,29 +1,38 @@
+"""
+Set of neural networks for use with NILM task.
+"""
+
 from keras.models import Sequential
-from keras.layers.convolutional import Convolution1D, MaxPooling1D
+from keras.layers.convolutional import Convolution1D
 from keras.layers.core import Dense, Flatten, Reshape
 
+
 class DenoisingAutoencoder(object):
-    
-    def __init__(self, windowSize):
-        self.windowSize = windowSize
-        self.size = (windowSize - 3) * 8
+    """
+    Neural network which implements a denoising auto-encoder. Inputs are
+    convoluted before being fed into an encoding layer. From the encoding layer
+    we learn to recover the original signal.
+    """
+    def __init__(self, window_size):
+        self.window_size = window_size
+        self.size = (window_size - 3) * 8
         self.model = Sequential()
-        
-        self.model.add(Convolution1D(8, 4, 'uniform', 'linear', border_mode='valid', 
-                                     subsample_length=1, input_dim=1, input_length=windowSize))
+
+        self.model.add(Convolution1D(8, 4, 'uniform', 'linear',
+                                     border_mode='valid', subsample_length=1,
+                                     input_dim=1, input_length=window_size))
         self.model.add(Flatten())
-        
-        self.model.add(Dense(output_dim=self.size, init='uniform', activation='relu'))
+        self.model.add(Dense(output_dim=self.size, init='uniform',
+                             activation='relu'))
         self.model.add(Dense(128, 'uniform', 'relu'))
         self.model.add(Dense(self.size, 'uniform', 'relu'))
         self.model.add(Reshape(dims=(self.size, 1)))
-        self.model.add(Convolution1D(1, 4, 'uniform', 'linear', border_mode='valid',
-                                     subsample_length=1, input_dim=1, input_length=self.size))
-        
-        self.model.compile(loss='mean_squared_error', optimizer='rmsprop')        
-        
-    def train(self, X, Y):
-        #model.fit(X, Y, batch_size=10, nb_epoch=1)
-        return
+        self.model.add(Convolution1D(1, 4, 'uniform', 'linear',
+                                     border_mode='valid', subsample_length=1,
+                                     input_dim=1, input_length=self.size))
 
-neuralNet = DenoisingAutoencoder(1000)  #compile example neural net
+        self.model.compile(loss='mean_squared_error', optimizer='rmsprop')
+
+    def train(self, aggregate_power, device_power):
+        """Train the network given the aggregate and device powers."""
+        self.model.fit(aggregate_power, device_power, batch_size=10, nb_epoch=1)
