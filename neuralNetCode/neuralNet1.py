bb4bdbdd554ad5c1906f0d23f4a8134cab58e58c
imccarten1 <imccarten@gmail.com>
2015-11-25 18:36:23 -0700

    Fixes an bug

--- neuralNetCode/neuralNet1.py
+++ neuralNetCode/neuralNet1.py
@@ -13,12 +13,12 @@ class DenoisingAutoencoder(object):
                                      subsample_length=1, input_dim=1, input_length=windowSize))
         self.model.add(Flatten())
         
-        self.model.add(Dense(output_dim=size, init='uniform', activation='relu'))
+        self.model.add(Dense(output_dim=self.size, init='uniform', activation='relu'))
         self.model.add(Dense(128, 'uniform', 'relu'))
-        self.model.add(Dense(size, 'uniform', 'relu'))
-        self.model.add(Reshape(dims=(size, 1)))
+        self.model.add(Dense(self.size, 'uniform', 'relu'))
+        self.model.add(Reshape(dims=(self.size, 1)))
         self.model.add(Convolution1D(1, 4, 'uniform', 'linear', border_mode='valid',
-                                     subsample_length=1, input_dim=1, input_length=size))
+                                     subsample_length=1, input_dim=1, input_length=self.size))
         
         self.model.compile(loss='mean_squared_error', optimizer='rmsprop')        
         
