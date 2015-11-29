86091ae8984d03c748416de786a0f2a33c80b638
Jesse Huard <jhuard@ualberta.ca>
2015-11-29 13:51:11 -0700

    NILM: Use column_stack to make indicator matrix in constant energy solver.

--- nilm/preprocess.py
+++ nilm/preprocess.py
@@ -9,7 +9,7 @@ from scipy.optimize import minimize
 from nilm.evaluation import mean_squared_error
 
 
-def solve_constant_energy(aggregated, *device_activations):
+def solve_constant_energy(aggregated, device_activations):
     """
     Invert the indicator matrix, solving for the constant energy of each
     device. We return the constant power for each device, and the mean squared
@@ -19,7 +19,7 @@ def solve_constant_energy(aggregated, *device_activations):
         """Objective function for the minimization."""
         return np.sum((total - np.dot(matrix, power)) ** 2)
 
-    matrix = np.hstack((np.transpose(d) for d in device_activations))
+    matrix = np.column_stack(device_activations)
 
     p0 = np.zeros(matrix.shape[1])
     bounds = [(0, np.inf)] * matrix.shape[1]
