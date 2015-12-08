b29b5b1d6805a1ff859396cb823e2b26cd7cee7f
Diusrex <diusrex@gmail.com>
2015-12-08 10:21:13 -0700

    Changed the evaluate function to accept arguments.

--- evaluate.py
+++ evaluate.py
@@ -1,19 +1,40 @@
 import os
 
+import argparse
 import numpy as np
 
-from nilm.evaluation import f_score, root_mean_squared_error
+from nilm.evaluation import f_score, root_mean_squared_error, mean_squared_error
 from nilm.network import DenoisingAutoencoder
 from nilm.timeseries import TimeSeries
 
+# Set up arg parser
+parser = argparse.ArgumentParser(description='Run evaluation on a previously generated neural network.')
+parser.add_argument('-a', '--aggregated', required=True,
+                    help='Aggregated power usage file.')
+                    
+# Need results folder, and aggregated data folder
+parser.add_argument('-d', '--dir', required=True,
+                    help='Directory containing device files.')
+parser.add_argument('-m', '--model', required=True,
+                    help='Directory containing neural network model files.')
+parser.add_argument('-c', '--channel', required=True, help='Device channel')
+parser.add_argument('-l', '--log', default='/tmp/agg.log',
+                    help='File to write log to.')
+parser.add_argument('-p', '--preprocess',
+                    choices=['raw','constant','interval','edge'],
+                    default='raw',
+                    help='Which preprocessing algorithm to use.')
+    
+args = parser.parse_args()
 
-channel = 'channel_6'
-result_file = '../results/house1/edge/%s' % channel
+channel = args.channel
+
+result_file = '%s/%s' % (args.model, channel)
 model_file = os.path.abspath(result_file + '.yml')
 weight_file = os.path.abspath(result_file + '.h5')
 
-device_file = '../out-data/house1/%s.dat' % channel
-agg_file = '../out-data/house1/aggregate.dat'
+device_file = '%s/%s.dat' % (args.dir, channel)
+agg_file = args.aggregated
 
 max_energy = np.float32(1422.0)
 
@@ -45,7 +66,7 @@ dev_windows = network.model.predict(agg_windows)
 dev_windows.shape = (len(agg_windows), window_size - 6)
 print dev_windows.shape
 
-
+totalError = 0
 for (i, window) in enumerate(dev_windows):
     if not any([e > np.float(25.0) for e in truth_windows[i]]):
         continue
@@ -60,4 +81,7 @@ for (i, window) in enumerate(dev_windows):
     print window
     print truth_windows[i]
     print 'Error: %s' % root_mean_squared_error(window, truth_windows[i])
+    totalError += mean_squared_error(window, truth_windows[i])
     #print f_score(t1, t2)
+
+print 'Error: %s %s' % (totalError, np.sqrt(totalError))
